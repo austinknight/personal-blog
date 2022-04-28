30a89416abe908e4615fc6bfe2696280ee5c42f5
Austin Knight <austinknight89@gmail.com>
2022-04-27 22:15:30 -0600

    Add mdsvex and postcss.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,3 +1,5 @@
 <h1>Who Dis?</h1>
 
-<p>This site is where I talk about what I work on, and my thoughts about web-related stuff. You can follow me on <a href="https://twitter.com/austinknight">Twitter</a> for updates!</p>
+<div>
+  <p>This site is where I talk about what I work on, and my thoughts about web-related stuff. You can follow me on <a href="https://twitter.com/austinknight">Twitter</a> for updates!</p>
+</div>
---
+++ src/routes/uses.md
@@ -0,0 +1,12 @@
+# Uses
+
+**Here's some stuff I use**
+
+- SvelteKit
+- VS Code
+- Emojis 😎
+
+```js
+// JavaScript goes here,
+// And will be syntax-highlighted!
+```
