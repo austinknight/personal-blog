7adf8cd6cf91da537b1ab6da0ca0233a0f70e49e
Austin Knight <austinknight89@gmail.com>
2022-06-16 15:53:58 -0600

    Finish configuring GH Pages

--- gh-pages.js
+++ gh-pages.js
@@ -3,11 +3,10 @@ import { publish } from 'gh-pages';
 publish(
 	'build', // path to public directory
 	{
-		branch: 'gh-pages',
 		repo: 'https://github.com/austinknight/personal-blog.git',
 		dotfiles: true
 	},
-	() => {
-		console.log('Deploy Complete!');
+	(e) => {
+		console.log('Deploy Complete! ', e);
 	}
 );
--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -24,8 +26,13 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: null
-		})
+			fallback: 'index.html',
+			precompress: false
+		}),
+		appDir: 'internal',
+		paths: {
+			base: dev ? '' : ''
+		}
 	}
 };
 
